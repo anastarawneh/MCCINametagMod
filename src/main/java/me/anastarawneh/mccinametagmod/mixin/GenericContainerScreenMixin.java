52f3e32431b59a260bbaf5c9331367705b8ee715
anastarawneh <anastarawneh2001@icloud.com>
2024-05-17 15:23:40 +0300

    Update to 1.20.5

--- src/main/java/me/anastarawneh/mccinametagmod/mixin/GenericContainerScreenMixin.java
+++ src/main/java/me/anastarawneh/mccinametagmod/mixin/GenericContainerScreenMixin.java
@@ -4,18 +4,14 @@ import me.anastarawneh.mccinametagmod.MCCINametagMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.JsonHelper;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,15 +19,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GenericContainerScreen.class)
 public class GenericContainerScreenMixin {
-    @Shadow @Final private int rows;
-    @Unique private GenericContainerScreenHandler handler;
     @Unique private Inventory inventory;
     @Unique private Text title;
     @Unique boolean processed = false;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     public void constructor(GenericContainerScreenHandler handler, PlayerInventory inventory, Text title, CallbackInfo ci) {
-        this.handler = handler;
         this.inventory = handler.getInventory();
         this.title = title;
         this.processed = false;
@@ -42,9 +35,9 @@ public class GenericContainerScreenMixin {
         if (!processed) {
             if (title.getString().contains("WARDROBE") && title.getString().contains("PROFILE")) {
                 if (inventory.getStack(49).getName().getString().equals("Air")) return;
-                NbtList lore = inventory.getStack(49).getNbt().getCompound("display").getList("Lore", NbtElement.STRING_TYPE);
-                String levelLine = lore.get(2).asString();
-                String level = JsonHelper.deserialize(levelLine).getAsJsonArray("extra").get(4).getAsJsonObject().get("text").getAsString();
+                String levelLine = inventory.getStack(49).getComponents().get(DataComponentTypes.LORE).lines().get(2).getString();
+                MCCINametagMod.LOGGER.info(levelLine);
+                String level = levelLine.split(" ")[1];
                 if (MCCINametagMod.FACTION_LEVEL != Integer.parseInt(level)) {
                     MCCINametagMod.FACTION_LEVEL = Integer.parseInt(level);
                     MCCINametagMod.getConfig().factionLevel = Integer.parseInt(level);
